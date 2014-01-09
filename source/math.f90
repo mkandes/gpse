b46190e81443b31f6b959d8607abf2ddce8e360d
Marty Kandes <mkandes@gmail.com>
2014-01-09 05:34:27 -0800

    gpse version 0.0.2. git now tracking Makefile. factorial function added to math module. factorial passed initial functional testing.

--- source/math.f90
+++ source/math.f90
@@ -27,11 +27,11 @@
 !
 ! COPYRIGHT
 !     
-!     Copyright (c) 2013 Martin Charles Kandes
+!     Copyright (c) 2014 Martin Charles Kandes
 !
 ! LAST UPDATED
 !
-!     Sunday, December 29th, 2013
+!     Thursday, January 9th, 2014
 !
 ! -------------------------------------------------------------------------
 
@@ -42,25 +42,74 @@
       IMPLICIT NONE
       PRIVATE
 
-      CHARACTER ( LEN = * ), PARAMETER :: VERSION_NUMBER = '0.0.1'
+      CHARACTER ( LEN = * ), PARAMETER :: VERSION_NUMBER = '0.0.2'
 
       REAL, PARAMETER, PUBLIC :: PI = 3.14159265358979323846264338327950288
 
       PUBLIC :: factorial
-      PUBLIC :: alaguerre
-      PUBLIC :: hermite
-      PUBLIC :: laguerre
+!      PUBLIC :: alaguerre
+!      PUBLIC :: hermite
+!      PUBLIC :: laguerre
+!     PUBLIC :: gamma
 
       CONTAINS
 
-         INTEGER RECURSIVE FUNCTION factorial ( n ) RESULT ( nFactorial ) 
+         INTEGER RECURSIVE FUNCTION factorial ( n ) RESULT ( nFactorial )
    
             IMPLICIT NONE
 
             INTEGER, INTENT ( IN ) :: n
 
+            nFactorial = 0
+
+            IF ( KIND ( n ) == INT32 ) THEN
+
+               IF ( n == 0 ) THEN
+
+                  nFactorial = 1
+
+               ELSE IF ( n >= 1 .AND. n <= 12 ) THEN
+
+                  nFactorial = n * factorial ( n - 1 )
+
+               ELSE
+
+                  nFactorial = -1
+                  !WRITE ( UNIT = ERROR_UNIT , FMT = * ) 'math : factorial :: ERROR - n must be an integer greater than or equal to 0, but less than or equal to 12 because KIND ( n ) = INT32'
+                  STOP
+
+               END IF
+
+            ELSE IF ( KIND ( n ) == INT64 ) THEN
+
+               IF ( n == 0 ) THEN
+
+                  nFactorial = 1 
+
+               ELSE IF ( n >= 1 .AND. n <= 20 ) THEN
+
+                  nFactorial = n * factorial ( n - 1 ) 
+
+               ELSE
+
+                  nFactorial = -1
+                  !WRITE ( UNIT = ERROR_UNIT , FMT = * ) 'math : factorial :: ERROR - n must be an integer greater than or equal to 0, but less than or equal to 20 because KIND ( n ) = INT64'
+                  STOP
+
+               END IF
+
+            ELSE
+
+               nFactorial = -1
+               !WRITE ( UNIT = ERROR_UNIT , FMT = * ) 'math : factorial :: ERROR - KIND ( n ) != INT32 .OR. INT64'
+               STOP
+
+            END IF
+
+            RETURN
+
          END FUNCTION
 
-      END MODULE MATH
+      END MODULE
 
 ! =========================================================================
